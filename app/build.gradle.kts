7e57d08e7c78c92c0cd77a7ba4746e6daf4e71de
spovondra <spovondra@gmail.com>
2023-12-07 03:02:14 +0100

    drobné opravy + cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,10 +51,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.room:room-runtime:2.6.0")
-    implementation("androidx.room:room-ktx:2.6.0")
-    annotationProcessor("androidx.room:room-compiler:2.6.0")
-    ksp("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
